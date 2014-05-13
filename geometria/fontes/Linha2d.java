caa2cde0cbb02a4fc76a422372638d7f342bcc13
Aline <alinegsantoss@gmail.com>
2014-05-13 19:17:59 -0300

    adicionando teste

--- geometria/fontes/Linha2d.java
+++ geometria/fontes/Linha2d.java
@@ -10,5 +10,18 @@ package geometria.fontes;
  */
 public class Linha2d {
 
+	private int x = 1;
+	private int y = 4;
+	
+	public int getX() {
+		// TODO Auto-generated method stub
+		return x;
+	}
+
+	public int getY() {
+		// TODO Auto-generated method stub
+		return y;
+	}
+
     
 }
--- geometria/testes/TestLinha2d.java
+++ geometria/testes/TestLinha2d.java
@@ -1,14 +1,21 @@
 package geometria.testes;
 
 import static org.junit.Assert.*;
+import geometria.fontes.Linha2d;
 
 import org.junit.Test;
 
 public class TestLinha2d {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented!!!");
+	public void testPontosLinha1() {
+		Linha2d linha1 = new Linha2d();
+		linha1.getX();
+		linha1.getY(); 
+		assertEquals("não é igual", 1, linha1.getX());
+		assertEquals("não é igual", 4, linha1.getY());
+
 	}
 
+	
 }

4e4961f4286e01f3e4ee0d24fcb83353198a4da6
Aline <alinegsantoss@gmail.com>
2014-05-13 18:45:56 -0300

    primeiro teste

---
+++ geometria/testes/TestLinha2d.java
@@ -0,0 +1,14 @@
+package geometria.testes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestLinha2d {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

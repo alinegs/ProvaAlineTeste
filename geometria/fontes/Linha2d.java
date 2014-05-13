892b70b78f8f9ce35b620f71000d02adfa505157
myusername <myemail@gmail.com>
2014-05-13 17:16:01 -0300

    Criando linha2d

---
+++ geometria/fontes/Linha2d.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geometria.fontes;
+
+/**
+ *
+ * @author Alunos
+ */
+public class Linha2d {
+    
+}
